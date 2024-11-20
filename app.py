f1e18180908f2d9f8486636bad3d6a2e7d314222
amirhs73 <amir.hosseini@ubiweb.ca>
2024-11-20 10:35:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 logo = "https://github.com/amirhs73/-google-sheets-streamlit/blob/main/Logo.jpeg"
-st.image(logo, width=200)
+st.image(logo, width=2000)
 # Load data and perform analysis
 st.title("Ubiweb Google Ads Predictor")
 
