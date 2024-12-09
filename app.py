e297e7c0b10e5bc93ade64258db80ee0950afa36
amirhs73 <amir.hosseini@ubiweb.ca>
2024-12-09 00:23:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,13 +12,7 @@ st.image(logo, width = 200)
 st.title("Google Ads Predictor")
 
 
-def load_model():
-    model_path = "random_forest_model.pkl"
-    with open(model_path, "rb") as file:
-     model = pickle.load(file)
-    return model
 
-model = load_model()
 
 
 
